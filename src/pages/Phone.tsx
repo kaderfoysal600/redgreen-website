a94b9e3b4e5dd7c9fa1f80658d203b69664d1026
orun-sunny <sunny.mukut@gmail.com>
2023-07-10 16:29:48 +0600

    errornotworking

--- src/pages/Phone.tsx
+++ src/pages/Phone.tsx
@@ -1,13 +1,27 @@
 import DeviceBanner from "../component/DeviceBanner/DeviceBanner";
-import DeviceProduct from "../component/DeviceProduct/DeviceProduct"
+import DeviceProduct from "../component/DeviceProduct/DeviceProduct";
 import DeviceService from "../component/DeviceService/DeviceService";
+import { useEffect, useState } from "react";
+import { products } from "../product.json";
 
 const Phone = () => {
+  console.log("products", products);
+
+  const [data, setData] = useState([]);
+  let arr: any = [];
+  const filteredProducts = products.filter((item: any) => {
+    arr = [item];
+    return item.category === "Phone";
+  });
+  useEffect(() => {
+    setData(arr);
+    console.log("filteredProducts", data);
+  }, []);
   return (
     <div>
       {/* <Banner /> */}
       <DeviceBanner />
-      <DeviceProduct/>
+      <DeviceProduct />
       <DeviceService></DeviceService>
     </div>
   );
