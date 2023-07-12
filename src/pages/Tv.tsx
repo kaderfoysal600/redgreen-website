1779af4019878507e204cb66110c6935e3877eeb
kaderfoysal600 <lulaarbax@gmail.com>
2023-07-12 10:06:13 +0600

    refactor

--- src/pages/Tv.tsx
+++ src/pages/Tv.tsx
@@ -1,7 +1,6 @@
 import DeviceBanner from "../component/DeviceBanner/DeviceBanner";
 import DeviceProduct from "../component/DeviceProduct/DeviceProduct";
 import DeviceService from "../component/DeviceService/DeviceService";
-// import { useEffect, useState } from "react";
 import { products } from "../product.json";
 
 const Phone = () => {
